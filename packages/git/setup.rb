11fa555839fac28cbb86fbe4005cfe7253d9e6a8
Frederico Benevides <fredbene@gmail.com>
2013-06-12 16:50:05 -0300

    Add commands for linux

--- packages/git/setup.rb
+++ packages/git/setup.rb
@@ -1,5 +1,10 @@
 def setup_git
-  system 'brew install git' if OS.mac?
+  if OS.mac?
+    system 'brew install git'
+  else
+    system 'sudo apt-get install git-core'
+  end
+
   copy_file :from => 'git/.gitconfig', :to => '~/'
   copy_file :from => 'git/.gitignore', :to => '~/'
 end
--- packages/zsh/setup.rb
+++ packages/zsh/setup.rb
@@ -5,10 +5,17 @@ def setup_zsh
   copy_folder :from => 'zsh/plugins', :to => '~/.oh-my-zsh/custom'
   
   puts '==> Updating the .zshrc to have the new plugins and theme'
+
+  command_plugin = 'gsed -i "s/plugins=(git)/plugins=(brew bundler gem git fredericobenevides)/g" ~/.zshrc'
+  command_theme  = 'gsed -i "s/robbyrussell/fredericobenevides/g" ~/.zshrc'
+
   if OS.mac?
     system 'brew install gnu-sed'
     
-    system 'gsed -i "s/robbyrussell/fredericobenevides/g" ~/.zshrc'
-    system 'gsed -i "s/plugins=(git)/plugins=(brew bundler gem git fredericobenevides)/g" ~/.zshrc'
+    system command_plugin
+    system command_theme
+  else
+    system command_plugin.gsub('gsed', 'sed')
+    system command_theme.gsub('gsed', 'sed')
   end
 end
