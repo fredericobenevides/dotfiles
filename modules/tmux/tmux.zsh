f6f19c0778bc25dbd8d9ee193b45219b0a70ba6b
Frederico Benevides <fredbene@gmail.com>
2015-05-18 11:05:08 -0300

    fixed the tmux environment variable

--- modules/tmux/tmux.zsh
+++ modules/tmux/tmux.zsh
@@ -1,4 +1,5 @@
 # Work around when opening tmux to set the new environment to fix the colors
-if [ "$TERM" ] && [ $TERM = "xterm-256color" ]; then
+export TERM="xterm-256color"
+if [ "$TMUX" ] && [ $TERM = "xterm-256color" ]; then
   export TERM="screen-256color"
 fi
