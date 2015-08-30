8233c07e08a2f51fd32208e175857c6ed532ce40
Frederico Benevides <fredbene@gmail.com>
2015-08-30 10:58:06 -0300

    fixed the script to not use empty variables

--- modules/tmux/tmux_clipboard.sh
+++ modules/tmux/tmux_clipboard.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 os=`uname -s`
+os_system="MAC"
 
-if [ $os == "Linux" ]; then
+if [ "$os" == "Linux" ]; then
   os_system="LINUX"
 fi
 
 default() {
-  if [ $os_system == "LINUX" ]; then
+  if [ "$os_system" == "LINUX" ]; then
     zsh;
   else
     reattach-to-user-namespace -l zsh;
@@ -15,7 +16,7 @@ default() {
 }
 
 copy() {
-  if [ $os_system == "LINUX" ]; then
+  if [ "$os_system" == "LINUX" ]; then
     tmux show-buffer | xclip -selection clipboard -i;
   else
     reattach-to-user-namespace pbcopy;
@@ -23,16 +24,16 @@ copy() {
 }
 
 paste() {
-  if [ $os_system == "LINUX" ]; then
+  if [ "$os_system" == "LINUX" ]; then
     xclip -selection clipboard -o | tmux load-buffer - && tmux paste-buffer;
   else
     reattach-to-user-namespace pbpaste;
   fi
 }
 
-if [ $1 = "copy" ]; then
+if [ "$1" = "copy" ]; then
   copy;
-elif [ $1 = "paste" ]; then
+elif [ "$1" = "paste" ]; then
   paste;
 else
   default;
