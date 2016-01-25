cd2a8c91e197feaeb972e60def74a77e1c3a1714
Frederico Benevides <fredbene@gmail.com>
2016-01-25 18:03:27 -0200

    Changed golang to install using the package from the site

--- modules/golang/setup.rb
+++ modules/golang/setup.rb
@@ -1,13 +1,22 @@
 def install_module
   install 'GOLANG' do
-    description 'Installing golang'
 
     when_os :mac do
+      description 'Installing golang'
       run 'brew install go'
     end
 
     when_os :linux do
-      run 'sudo apt-get install golang'
+      version = '1.5.3'
+
+      description 'Installing golang'
+      run "wget -q -O- https://storage.googleapis.com/golang/go#{version}.linux-amd64.tar.gz | tar xzvf - -C /tmp/"
+
+      description 'Moving the go folder to /opt'
+      run "sudo mv /tmp/go /opt"
+
+      description 'Exporting go path'
+      run 'grep -q "/opt/go/bin" ~/.zshrc.local; if [ $? -ne 0 ];then echo "export GOROOT=\"/opt/go\"" >> ~/.zshrc.local; echo "export PATH=\"/opt/go/bin:\$PATH\"" >> ~/.zshrc.local; fi'
     end
   end
 end
@@ -21,7 +30,13 @@ def uninstall_module
     end
 
     when_os :linux do
-      run 'sudo apt-get purge golang'
+      version = '1.5.3'
+
+      description 'Uninstalling golang'
+      run 'sudo rm -rf /opt/go'
+
+      description 'Removing go path'
+      run 'sed -i "/\/opt\/go/d" ~/.zshrc.local'
     end
   end
 end
