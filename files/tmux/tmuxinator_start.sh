c50ea6365923d5bc5ef7b2989da43e719ccf81f7
Frederico Benevides <fredbene@gmail.com>
2017-07-16 20:21:50 -0300

    Create the install for tmux

---
+++ files/tmux/tmuxinator_start.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+export PATH=$PATH:/usr/local/bin
+
+clear
+
+# present menu for user to choose which workspace to open
+PS3="Please choose your tmuxinator: "
+options=("zsh" "New tmuxinator" $(tmuxinator list | tail -n +2) )
+
+echo "Available tmuxinator"
+echo "------------------"
+echo " "
+select opt in "${options[@]}"
+do
+  case $opt in
+    "New tmuxinator")
+      read -p "Enter new tmuxinator name: " tmuxinator_name
+      tmuxinator open "$tmuxinator_name"
+      break
+      ;;
+    "zsh")
+      zsh
+      break
+      ;;
+    *)
+      echo "------------------"
+      echo "Run or Edit?"
+      echo "------------------"
+      echo " "
+
+      subOptions=("run" "edit")
+      select  subOptions in "${subOptions[@]}"
+      do
+        case $subOptions in
+          "run")
+            tmuxinator $opt
+            break
+            ;;
+          "edit")
+            tmuxinator open $opt
+            break
+            ;;
+        esac
+      done
+      break
+      ;;
+  esac
+done
