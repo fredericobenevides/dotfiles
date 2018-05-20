d7cf027755d96f6558f49459c44d653f03920fc9
Frederico Benevides <fredbene@gmail.com>
2018-05-20 01:11:16 -0300

    change to use neovim

--- files/vim/install.sh
+++ files/vim/install.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+
+set -e
+
 install_vim_plug() {
   echo "Installing the plugin vim-plug to manage the vim plugins"
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+install_neovim_plug() {
+  echo "Installing the plugin vim-plug to manage the vim plugins"
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
 install_matchit_plugin() {
@@ -18,8 +27,28 @@ launch_vim_to_install_all_plugins() {
   vim +PlugInstall +qall
 }
 
-install_matchit_plugin
+launch_neovim_to_install_all_plugins() {
+  echo "Launching neovim to install all plugins"
+  nvim +PlugInstall +qall
+}
 
-install_vim_plug
-launch_vim_to_install_all_plugins
+setup_vim() {
+  install_matchit_plugin
 
+  install_vim_plug
+  launch_vim_to_install_all_plugins
+}
+
+setup_neovim() {
+  pip install neovim
+
+  mkdir -p $HOME/.config/nvim
+
+  nvim=`which nvim`
+  if [ ! -n $nvim ]; then
+    install_neovim_plug
+    launch_neovim_to_install_all_plugins
+  fi
+}
+
+setup_neovim
