6a7cdfb7426a3d1f7817ae63efc67cf14a6f4172
Frederico Benevides <fredbene@gmail.com>
2019-06-30 00:34:19 -0300

    Moved config files to the folder config_files and installation files to the folder install_files.

--- install_files/install_anaconda_linux.sh
+++ install_files/install_anaconda_linux.sh
@@ -1,15 +1,22 @@
 #! /bin/bash -x
 
-filename="Anaconda3-5.2.0-Linux-x86_64.sh"
+filename="Anaconda3-2019.03-Linux-x86_64.sh"
 
 install_anaconda() {
   anaconda=`python -V 2>&1 | grep Anaconda`
-  if [ -z "$anaconda" ]; then
+  if [[  -d "/opt/anaconda3" ]]; then
+    echo "Already installed"
+  else
     curl -L https://repo.anaconda.com/archive/${filename} -o /tmp/$filename
     chmod +x /tmp/$filename
     sudo /tmp/$filename -b -p /opt/anaconda3
     sudo chown -R `whoami`:`whoami` /opt/anaconda3
+
+    rm /tmp/$filename
+
   fi
+
+  exit 0
 }
 
 install_anaconda
