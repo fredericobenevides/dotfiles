542222436e300925c5dd919b477286fdc3b3034f
Frederico Benevides <fredbene@gmail.com>
2019-08-21 15:33:31 -0300

    improved the kvm setup

--- other/setup_kvm.sh
+++ other/setup_kvm.sh
@@ -4,3 +4,15 @@ sudo pacman -S virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netc
 
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
+
+sudo usermod -a -G kvm,libvirt $(whoami)
+
+newgrp libvirt
+newgrp kvm
+
+curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 -o /tmp/docker-machine-driver-kvm
+sudo mv /tmp/docker-machine-driver-kvm /usr/local/bin
+sudo chmod +x /usr/local/bin/docker-machine-driver-kvm
+
+sudo virsh net-start default
+sudo virsh net-autostart default
