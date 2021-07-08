d57687d633a6ae02ea9a45ce3823d3fefe97c772
Frederico Benevides <fredbene@gmail.com>
2021-07-07 21:02:24 -0300

    update the app's version

--- other/setup_companion_app.sh
+++ other/setup_companion_app.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-curl -Lo /tmp/net.downloadhelper.coapp-1.6.1-1_amd64.tar.gz https://github.com/mi-g/vdhcoapp/releases/download/v1.6.1/net.downloadhelper.coapp-1.6.1-1_amd64.tar.gz
+VERSION=1.6.3
 
-tar xf /tmp/net.downloadhelper.coapp-1.6.1-1_amd64.tar.gz -C /tmp
-/tmp/net.downloadhelper.coapp-1.6.1/bin/net.downloadhelper.coapp-linux-64 install --user
+curl -Lo /tmp/net.downloadhelper.coapp-$VERSION-1_amd64.tar.gz https://github.com/mi-g/vdhcoapp/releases/download/v$VERSION/net.downloadhelper.coapp-$VERSION-1_amd64.tar.gz
+
+tar xf /tmp/net.downloadhelper.coapp-$VERSION-1_amd64.tar.gz -C /tmp
+/tmp/net.downloadhelper.coapp-$VERSION/bin/net.downloadhelper.coapp-linux-64 install --user
