297eefcfa2cfe0a8dc04d61869d5bb1857681e51
Frederico Benevides <fredbene@gmail.com>
2021-09-07 21:23:50 -0300

    Updated configs for doom-emacs

--- files/emacs/doom.d/config.el
+++ files/emacs/doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Frederico Benevides"
+      user-mail-address "contato@fredericobenevides.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -21,6 +21,8 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "JetBrainsMono NerdFont" :size 16)
+      doom-variable-pitch-font (font-spec :family "sans" :size 12))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
