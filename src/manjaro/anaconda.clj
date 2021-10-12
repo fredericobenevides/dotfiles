b491004512ad4f54d0b461d8e197113fc803e7f4
Frederico Benevides <fredbene@gmail.com>
2021-10-12 14:47:27 -0300

    Refactored the anaconda installation and simplified with the help of the utils package

--- src/manjaro/anaconda.clj
+++ src/manjaro/anaconda.clj
@@ -1,5 +1,6 @@
 (ns manjaro.anaconda
-  (:require [babashka.process :refer [process sh]]
+  (:require [utils]
+            [babashka.process :refer [process sh]]
             [babashka.fs :as fs]
             [babashka.curl :as curl]
             [clojure.java.io :as io]
@@ -8,70 +9,29 @@
 (def anaconda-url "https://repo.anaconda.com/archive")
 (def anaconda-file "Anaconda3-2021.05-Linux-x86_64.sh")
 
-(defn empty-folder?
-  "Verify if the folder is empty"
-  [folder]
-  (not (fs/exists? folder)))
-
-(defn download-file
-  "Download the file from a url to a specific folder"
-  [url destination]
-    (io/copy 
-      (:body (curl/get url {:as :stream})) 
-             (io/file destination)))
-
-(defn run-shell
-  [cmd]
-  (-> @(process cmd {:err :inherit :in :inherit :out :string}) :out))
-
-(defn download-anaconda-file
+(defn install-anaconda
   []
-  (println "Downloading anaconda files")
-  (let [empty-folder (empty-folder? "/opt/anaconda3")
+  (println "Installing anaconda on the system")
+  (let [path (str "/tmp/" anaconda-file)
+        chmod-cmd (str "chmod +x " path)
+        install-cmd (str "sudo " path " " "-b -p /opt/anaconda3")]
+    (utils/run-shell chmod-cmd)
+    (utils/run-shell-stream install-cmd)
+    (utils/remove-file (str "/tmp/" anaconda-file))))
+
+(defn run
+  []
+  (let [empty-folder (utils/empty-folder? "/opt/anaconda3")
         url-file (str anaconda-url "/" anaconda-file)
         dest-file (str "/tmp/" anaconda-file)]
     (if empty-folder
       (do
-        (download-file url-file dest-file)))))
-
-(defn install-anaconda
-  []
-  (println "Installing anaconda on the system")
-  (let [chmod-cmd (str "chmod +x " "/tmp/" anaconda-file)
-        install-cmd (str "sudo " "/tmp/" anaconda-file " " "-b -p /opt/anaconda3")]
-    (run-shell chmod-cmd)
-    (run-shell install-cmd)))
-
-(defn fix-permission
-  [location]
-  (println "Fixing permission at" location)
-  (let [username (str/trim (run-shell "whoami"))
-        group (str/trim (run-shell "id -g"))
-        permission-cmd (str "sudo chown -R " username ":" group " " location)]
-    (run-shell permission-cmd)))
-
-(defn link-files
-  "Create a link between a file and link"
-  [link file]
-  (println "Creating a link from" link "to" file)
-  (let [home (System/getProperty "user.home")
-        home-link (str home "/" link)
-        home-file (str home "/" file)]
-    (try 
-      (fs/create-sym-link home-link home-file)
-      (catch java.nio.file.FileAlreadyExistsException e (println "Link já foi adicionado anteriormente")))))
-
-(defn run
-  []
-  (let [empty-folder (empty-folder? "/opt/anaconda3")]
-    (if empty-folder
-      (do
         (println "\nInstalling Anaconda")
 
-        (download-anaconda-file)
+        (utils/download-file url-file dest-file)
         (install-anaconda)
-        (fix-permission "/opt/anaconda3")
-        (link-files ".pythonrc" ".dotfiles/files/python/.pythonrc")
+        (utils/fix-permission "/opt/anaconda3")
+        (utils/link-files ".pythonrc" ".dotfiles/files/python/.pythonrc")
 
         (println "Finished the installation of Anaconda"))
-      (println "\nSkipping the installation of Anaconda"))))
+        (println "\nSkipping the installation of Anaconda"))))
--- src/utils.clj
+++ src/utils.clj
@@ -1,5 +1,58 @@
 (ns utils
-  (:require [babashka.process :refer [process]]))
+  (:require [babashka.process :refer [process]]
+            [babashka.fs :as fs]
+            [babashka.curl :as curl]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn run-shell
+  "Run shell allows the user to run a command directly to the shell and get the response after the execution is finished"
+  [cmd]
+  (-> @(process cmd {:err :inherit :in :inherit :out :string}) :out))
+
+(defn run-shell-stream
+  "Run shell stream allows the user to run a command directly to the shell and get the output during the stream"
+  [cmd]
+  (-> @(process cmd {:err :inherit :in :inherit :out :inherit})))
+
+(defn empty-folder?
+  "Verify if the folder is empty"
+  [folder]
+  (not (fs/exists? folder)))
+
+(defn download-file
+  "Download the file from a url to a specific folder"
+  [url destination]
+  (println "Downloading file from" url "to" destination)
+  (io/copy 
+    (:body (curl/get url {:as :stream})) 
+           (io/file destination)))
+
+(defn remove-file
+  "Allow to remove a file from a system"
+  [path]
+  (println "Removing a file from" path)
+  (fs/delete-if-exists path))
+
+(defn fix-permission
+  "Allow to fix the permission of a path using the username and user's group"
+  [path]
+  (println "Fixing permission at" path)
+  (let [username (str/trim (run-shell "whoami"))
+        group (str/trim (run-shell "id -g"))
+        permission-cmd (str "sudo chown -R " username ":" group " " path)]
+    (run-shell permission-cmd)))
+
+(defn link-files
+  "Create a link between a file and link"
+  [link file]
+  (println "Creating a link from" link "to" file)
+  (let [home (System/getProperty "user.home")
+        home-link (str home "/" link)
+        home-file (str home "/" file)]
+    (try 
+      (fs/create-sym-link home-link home-file)
+      (catch java.nio.file.FileAlreadyExistsException e (println "Link already added")))))
 
 (defn- exist-pkg?
   "Verify if the package exist"
@@ -14,4 +67,5 @@
       (println "Skippping package" pkg "already installed")
       (do
         (println description)
-        (-> @(process install-cmd {:err :inherit :in :inherit :out :inherit})))))
+        (run-shell-stream install-cmd))))
+
