fcff0f69c1d4a00c0b6942abd7c45cfab3d6d5a3
Frederico Benevides <fredbene@gmail.com>
2022-01-15 01:30:25 -0300

    Improved the performance of lsp-mode

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -290,7 +290,14 @@
   ;; set prefix for lsp-command-keymap
   (setq lsp-keymap-prefix "C-c l")
   :hook
-    ((lsp-mode . lsp-enable-which-key-integration)))
+    ((lsp-mode . lsp-enable-which-key-integration))
+  :config
+  (setq gc-cons-threshold (* 100 1024 1024)
+    read-process-output-max (* 1024 1024)
+    treemacs-space-between-root-nodes nil
+    company-idle-delay 0.0
+    company-minimum-prefix-length 1
+    lsp-idle-delay 0.1))
 
 (use-package lsp-ui :commands lsp-ui-mode)
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
