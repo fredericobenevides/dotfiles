3de406e904d1fe9d7943a3c35ab37766a6a05ecb
Frederico Benevides <fredbene@gmail.com>
2022-02-23 18:15:09 -0300

    include the xrandr.sh file

---
+++ files/i3/xrandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 640x1080 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 2560x132 --rotate left --output DP-5 --off
