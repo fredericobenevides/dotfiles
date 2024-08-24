742211f29b8e9d6100e5ca9a14793b399a1a80fc
Frederico Benevides <frederico@fredericobenevides.com>
2024-08-24 20:20:42 -0300

    Add the command to bind caps lock to ctrl inside .xprofile

--- modules/xorg.sh
+++ modules/xorg.sh
@@ -10,6 +10,7 @@ info "Installing and configuring xorg"
 echo -e "-- Linking files"
 ln -sf ~/.dotfiles/files/xorg/.conkyrc ~/.conkyrc
 ln -sf ~/.dotfiles/files/xorg/.XCompose ~/.XCompose
+ln -sf ~/.dotfiles/files/xorg/.xprofile ~/.xprofile
 
 #ln -sf ~/.dotfiles/files/xorg/.xbindkeysrc ~/.xbindkeysrc
 #ln -sf ~/.dotfiles/files/xorg/.xprofile ~/.xprofile
