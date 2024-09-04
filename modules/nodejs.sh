645229eb00c844908c617371bcd380a2a2108258
Frederico Benevides <frederico@fredericobenevides.com>
2024-09-04 16:06:22 -0300

    Updated to fnm

--- modules/nodejs.sh
+++ modules/nodejs.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
-if [ -d ~/.volta ]; then
-    info "nodejs already configured!"
-    return
-fi
+function install_nodejs() {
+    if [ -d ~/.fnm ]; then
+        info "nodejs already configured!"
+        return
+    fi
 
-info "Installing and configuring nodejs"
+    info "Installing and configuring nodejs"
 
-echo -e "-- Install volta to manage nodejs"
-curl https://get.volta.sh | bash
+    echo -e "-- Installing fnm to manage nodejs"
+    curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir $HOME/.fnm --skip-shell
 
-echo -e "-- Installing nodejs"
-sh -c "(volta install node)"
+    echo -e "-- Preparing fnm environment"
+    export PATH=$HOME/.fnm:$PATH
+    eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
 
-info "Finished installing and configuring nodejs"
+    echo -e "-- Installing nodejs"
+    fnm install v20.17.0
+
+    echo -e "-- Setting default nodejs"
+    fnm default v20.17.0
+
+    info "Finished installing and configuring nodejs"
+}
+
+function install_pkgs_nodejs() {
+  info "Installing nodejs packages"
+
+  npm "vscode-langservers-extracted"
+
+  info "Finished installing nodejs packages"
+}
+
+install_nodejs
+install_pkgs_nodejs
--- modules/utils.sh
+++ modules/utils.sh
@@ -14,6 +14,16 @@ function flatpak() {
   fi
 }
 
+function npm() {
+    bash -c "npm -g list 2>&1 | grep $1 > /dev/null"
+    if [ $? -eq 1 ]; then
+      echo -e "-- Installing $1"
+      bash -c "npm install -g $1"
+    else
+      echo -e "-- Package $1 is already installed"
+    fi
+}
+
 function pacman() {
   /usr/bin/pacman -Qi $1 > /dev/null 2>&1
   if [ $? -eq 1 ]; then
