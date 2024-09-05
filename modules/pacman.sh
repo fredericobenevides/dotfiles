eab594a00d43f16c44e6eca02fe91410270e1525
Frederico Benevides <frederico@fredericobenevides.com>
2024-09-04 22:24:47 -0300

    Fixed the order

--- modules/pacman.sh
+++ modules/pacman.sh
@@ -7,7 +7,7 @@ pacman "cmake"
 pacman "conky"
 pacman "flameshot"
 pacman "gdb"
-pacman "zathura-pdf-mupdf"
 pacman "xdotool"
+pacman "zathura-pdf-mupdf"
 
 info "Finished installing pacman packages"
