6b9578c2dbbd03be74432dea2054329d7bc7c34a
Frederico Benevides <frederico@fredericobenevides.com>
2024-12-03 00:27:59 -0300

    added new plugins

--- modules/vscode.sh
+++ modules/vscode.sh
@@ -17,13 +17,39 @@ function install_vscode() {
 function install_vscode_extensions() {
     info "Installing vscode extensions"
 
+    # editor
+    code "alefragnani.Bookmarks"
+    code "ritwickdey.LiveServer"
+    code "tuttieee.emacs-mcx"
     code "usernamehw.find-jump"
-    
+    code "tamasfe.even-better-toml"    
+    code "vadimcn.vscode-lldb"
+    code "wayou.vscode-todo-highlight"
+
+    # colors
+    code "kamikillerto.vscode-colorize"
+    code "oderwat.indent-rainbow"
+    code "vscode-icons-team.vscode-icons"
+
+    # docker
+    code "ms-azuretools.vscode-docker"
+
+    # git
+    code "donjayamanne.githistory"    
+    code "eamodio.gitlens"
+
+    # html, css, javascript, angular
     code "angular.ng-template"
     code "gydunhn.angular-essentials"
-
+    code "xabikos.JavaScriptSnippets"
+    code "formulahendry.auto-rename-tag"
+    code "pranaygp.vscode-css-peek"
+    code "bradlc.vscode-tailwindcss"
+    code "dbaeumer.vscode-eslint"
     code "esbenp.prettier-vscode"
 
+    # rust
+    code "serayuzgur.crates"
     code "rust-lang.rust-analyzer"
 
     info "Finished installing vscode extensions"
