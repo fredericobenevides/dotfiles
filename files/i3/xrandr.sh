02a6644f94563de46216bb3c2715a8e51ac5366a
Frederico Benevides <frederico@fredericobenevides.com>
2024-12-03 22:21:14 -0300

    Updated workspace configs and monitor settings

--- files/i3/xrandr.sh
+++ files/i3/xrandr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1920x0 --rotate right --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 3000x420 --rotate normal --output DP-5 --off
+xrandr --output HDMI-0 --mode 2560x1080 --pos 1080x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x240 --rotate right --refresh 164.92 --output DP-3 --off --output DP-4 --primary --mode 1920x1080 --pos 1080x1080 --rotate normal --refresh 164.92 --output DP-5 --off
