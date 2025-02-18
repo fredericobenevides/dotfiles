5f46feaa5c09774f2d3ed8d26b904640e7d7e3b3
Frederico Benevides <frederico@fredericobenevides.com>
2025-02-18 16:04:33 -0300

    removed files

--- other/setup_companion_app.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=1.6.3
-
-curl -Lo /tmp/net.downloadhelper.coapp-$VERSION-1_amd64.tar.gz https://github.com/mi-g/vdhcoapp/releases/download/v$VERSION/net.downloadhelper.coapp-$VERSION-1_amd64.tar.gz
-
-tar xf /tmp/net.downloadhelper.coapp-$VERSION-1_amd64.tar.gz -C /tmp
-/tmp/net.downloadhelper.coapp-$VERSION/bin/net.downloadhelper.coapp-linux-64 install --user
