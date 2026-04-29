6ca160358b5d0d8924567736465a603139a2c64f
Frederico Benevides <frederico@fredericobenevides.com>
2026-04-29 10:26:36 -0300

    added hyprlock, hyprlidle. Changed some configs in hyprland

--- makefiles/hyperland.mk
+++ makefiles/hyperland.mk
@@ -1,7 +1,7 @@
 HYPR_CONFIG_DIR := $(HOME)/.config/hypr
 HYPR_DOTFILES   := $(HOME)/.dotfiles/files/hyprland
 
-HYPR_PKGS := wayland waybar hyprland hyprpicker rofi-wayland grim slurp imv swayidle swaync
+HYPR_PKGS := wayland waybar hyprland hypridle hyprpicker hyprlock rofi-wayland grim slurp imv swaync brightnessctl
 
 hyprland-all:
 	@pacman -Q hyprland > /dev/null 2>&1 && test -d $(HYPR_CONFIG_DIR) && echo "#### Hyprland already configured!" || $(MAKE) hyprland-setup
@@ -12,11 +12,10 @@ hyprland-setup:
 	@echo "-- Installing system packages"
 	sudo pacman -S --needed --noconfirm $(HYPR_PKGS)
 	
-	@echo "-- Installing AUR packages"
-	yay -S --needed --noconfirm swaylock-effects-git
-	
 	@echo "-- Creating directory $(HYPR_CONFIG_DIR)"
 	mkdir -p $(HYPR_CONFIG_DIR)
 	
 	@echo "-- Linking files"
 	ln -sf $(HYPR_DOTFILES)/hyprland.conf $(HYPR_CONFIG_DIR)/hyprland.conf
+	ln -sf $(HYPR_DOTFILES)/hypridle.conf $(HYPR_CONFIG_DIR)/hypridle.conf
+	ln -sf $(HYPR_DOTFILES)/hyprlock.conf $(HYPR_CONFIG_DIR)/hyprlock.conf
